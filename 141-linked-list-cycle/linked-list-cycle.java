494dcb21daf2039fb5ab33ba643398f679eacfc0
bnr4012 <bnr4012@gmail.com>
2026-07-17 15:07:08 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (72.95%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,13 +11,23 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        HashSet<ListNode> set= new HashSet<>();
-        while(head!=null){
-            if(set.contains(head)){
+        // HashSet<ListNode> set= new HashSet<>();
+        // while(head!=null){
+        //     if(set.contains(head)){
+        //         return true;
+        //     }
+        //     set.add(head);
+        //     head=head.next;
+        // }
+        // return false;
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
                 return true;
             }
-            set.add(head);
-            head=head.next;
         }
         return false;
     }
